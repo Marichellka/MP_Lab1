ea1cc261d3d023882fd8cde8af9790ee95ae5592
Marichka <marichka.v.richtsi@gmail.com>
2022-02-03 22:53:29 +0200

    add reading
    with some bugs)))

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Task2
 {
@@ -6,7 +7,148 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inPath = "input.txt";
+            string outPath = "output.txt";
+            string[] words = new string[0];
+            int[] counts = new int[0];
+            int[][] pages = new int[0][];
+            int currentPage = 1;
+            int length = 0, i, strCount = 0;
+            int pageLinesCount = 45;
+            string word = "";
+            StreamReader reader = new StreamReader(inPath);
+            readFile:
+            {
+                if (reader.EndOfStream)
+                    goto endReading;
+
+                string str = reader.ReadLine();
+                if (strCount == pageLinesCount)
+                {
+                    currentPage++;
+                    strCount = 0;
+                }
+                strCount++;
+
+                int j = 0;
+                For1:
+                {
+                    char symbol = str[j];
+                    if ('Z' >= symbol && symbol >= 'A')
+                    {
+                        word += ((char)(symbol + 32)).ToString();
+                        if (!reader.EndOfStream)
+                            goto For1;
+                    }
+                    else if ('z' >= symbol && symbol >= 'a')
+                    {
+                        word += symbol;
+                        if (!reader.EndOfStream)
+                            goto For1;
+                    }
+
+                    if (word != "")
+                    {
+                        i = 0;
+                        checkWords:
+                        {
+                            if (i == length)
+                                goto newWord;
+                            if (word == words[i])
+                            {
+                                counts[i]++;
+                                word = "";
+                                if (counts[i] <= pages[i].Length)
+                                {
+                                    pages[i][counts[i] - 1] = currentPage;
+                                }
+                                else
+                                {
+                                    int[] pagesTmp = new int[counts[i] * 2];
+                                    int k = 0;
+                                    copyPages:
+                                    {
+                                        pagesTmp[k] = pages[i][k];
+                                        k++;
+                                        if (k < counts[i])
+                                            goto copyPages;
+                                    }
+                                }
+                                /*if (j==str.Length-1)???
+                                    goto ???;*/
+
+                                goto For1;
+                            }
+                            i++;
+                            goto checkWords;
+                        }
+
+                        newWord:
+                        if (length != words.Length)
+                        {
+                            words[length] = word;
+                            counts[length] = 1;
+                            pages[length] = new int[] { currentPage };
+                            length++;
+                            /*if (j==str.Length-1)???
+                                    goto ???;*/
+
+                            goto For1;
+                        }
+
+                        string[] newWords = new string[length * 2];
+                        int[] newCounts = new int[length * 2];
+                        int[][] newPages = new int[length * 2][];
+
+                        i = 0;
+                        forCopy:
+                        {
+                            if (i == length)
+                                goto endCopy;
+                            newWords[i] = words[i];
+                            newCounts[i] = counts[i];
+                            newPages[i] = pages[i];
+                            i++;
+                            goto forCopy;
+                        }
+
+                        endCopy:
+                        newWords[length] = word;
+                        newCounts[length] = 1;
+                        newPages[length] = new int[] { currentPage };
+                        words = newWords;
+                        counts = newCounts;
+                        pages = newPages;
+                        word = "";
+                        length++;
+                    }
+
+                    if(j!=str.Length-1)
+                        goto For1;
+                }
+                
+
+                if (!reader.EndOfStream)
+                    goto readFile;
+            }
+
+            endReading:
+            reader.Close();
+
+            //sorting
+
+            //output
+            StreamWriter writer = new StreamWriter(outPath);
+            i = 0;
+            write:
+            {
+                writer.WriteLine(words[i] + " - " + counts[i]);
+                i++;
+                if (i < length)
+                    goto write;
+            }
+
+            writer.Close();
         }
     }
 }
